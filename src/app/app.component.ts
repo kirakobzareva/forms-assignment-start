97ce7bde4f0eb651f46227995ee1364d0410ce2d
Kira Kobzareva <kirakobzareva@gmail.com>
2022-07-16 11:31:39 +0200

    add assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'forms-assignment-start';
+  defaultValue = 'advanced';
+  @ViewChild('f') signupForm!: NgForm;
+  submited = false;
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
